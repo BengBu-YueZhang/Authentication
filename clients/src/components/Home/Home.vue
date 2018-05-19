6008d9f24dc0488fbb917320cbeaf73e1cb1bd58
张越 <zhangyue@zhangyuedeMacBook-Pro.local>
2018-05-19 21:17:45 +0800

    【init】初始化客户端代码

--- clients/src/components/Home/Home.vue
+++ clients/src/components/Home/Home.vue
@@ -1,12 +1,63 @@
 <template>
-  <div>Home</div>
+  <section class="home-wrapper">
+    <div class="content">
+      <div class="login" v-show="!visible">
+        <mu-text-field hintText="用户名" fullWidth />
+        <mu-text-field hintText="密码" type="password" fullWidth/>
+        <mu-raised-button label="登录" fullWidth primary/>
+        <div class="line"></div>
+        <mu-raised-button label="注册" fullWidth secondary @click="visible = !visible"/>
+      </div>
+      <div class="registered" v-show="visible">
+        <mu-text-field hintText="用户名" fullWidth />
+        <mu-text-field hintText="密码" type="password" fullWidth/>
+        <mu-text-field hintText="再次输入密码" type="password" fullWidth/>
+        <mu-raised-button label="注册" fullWidth primary/>
+        <div class="line"></div>
+        <mu-raised-button label="登录" fullWidth secondary @click="visible = !visible"/>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
 export default {
-  name: 'Home'
+  name: 'Home',
+
+  data () {
+    return {
+      visible: false
+    }
+  }
 }
 </script>
 
 <style scoped>
+.home-wrapper {
+  position: relative;
+  width: 100%;
+  height: 100vh;
+  min-height: 100%;
+  overflow: hidden;
+  background: url(https://unsplash.it/1920/1080?random);
+  background-size: cover;
+}
+
+.content {
+  position: absolute;
+  right: 0;
+  top: 0;
+  background-color: rgba(255, 255, 255, 0.9);
+  width: 320px;
+  height: 100vh;
+  min-height: 100%;
+  overflow: hidden;
+  box-sizing: border-box;
+  padding: 30px 20px;
+}
+
+.line {
+  width: 100%;
+  height: 10px;
+}
 </style>
--- clients/src/components/User/User.vue
+++ clients/src/components/User/User.vue
@@ -1,5 +1,9 @@
 <template>
-  <div>User</div>
+  <mu-appbar title="详情">
+    <mu-icon-menu icon="more_vert" slot="right">
+      <mu-menu-item title="退出"/>
+    </mu-icon-menu>
+  </mu-appbar>
 </template>
 
 <script>
