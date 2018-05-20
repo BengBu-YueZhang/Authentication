4eb9ec8d06d5a3487587c800f83d8f36afaa4b69
张越 <zhangyue@zhangyuedeMacBook-Pro.local>
2018-05-20 09:44:29 +0800

    【Add】添加token鉴权中间件

---
+++ token/server/middleware/AuthenticationToken.js
@@ -0,0 +1,19 @@
+const jwt = require('jsonwebtoken')
+const secret = require('../config/index').secret
+
+module.exports = function (req, res, next) {
+  const token = req.body.token || req.query.token || req.headers['x-access-token']
+  if (token) {
+    jwt.verify(token, secret, function(err, decoded) {      
+      if (err) {
+        return res.json({ code: 'error', error: 'token失效' })    
+      } else {
+        console.log(decoded)
+        req.decoded = decoded    
+        next()
+      }
+    })
+  } else {
+    res.status(403).json({code: 'error', error: '没有权限'})
+  }
+}
